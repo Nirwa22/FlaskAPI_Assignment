c16638cff9b1f520c219cfa59ad6b669a711c642
Nirwa22 <nyusra.msai23seecs@seecs.edu.pk>
2024-09-30 15:54:29 +0500

    GET USER INFORMATION API added

--- Flask.py
+++ Flask.py
@@ -1,8 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+
+
 @app.route("/")
 def home():
     return "Home Route"
 
+
+@app.route("/User_Information")
+def user_data():
+    user_data = [
+                {"Name": "Sarah", "Age": 34, "Gender": "f"},
+                {"Name": "Maha", "Age": 15, "Gender": "f"},
+                {"Name": "Ali", "Age": 40, "Gender": "m"},
+                {"Name": "Tahir", "Age": 63, "Gender": 'm'},
+                {"Name": "Ayesha", "Age": 25, "Gender": "f"},
+                {"Name": "Zahid", "Age": 23, "Gender": "m"},
+             ]
+    return jsonify(user_data)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
