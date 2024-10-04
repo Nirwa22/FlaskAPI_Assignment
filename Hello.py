e8484d8707fb41aee896f230f17c6239a795e39a
Nirwa22 <nyusra.msai23seecs@seecs.edu.pk>
2024-10-04 16:38:50 +0500

    ALL Four routes added

--- Hello.py
+++ Hello.py
@@ -1,39 +1,27 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
 from flask_cors import CORS
 from dotenv import load_dotenv
-import os, uuid
+import os
+import uuid
+import json
 
 load_dotenv()
 API_KEY = os.getenv("SECRET_API_KEY")
 app = Flask(__name__)
 CORS(app)
 
-session_file = {}
-users = [
-                {"id": 1, "Name": "Sarah", "Age": 34, "Gender": "f"},
-                {"id": 2, "Name": "Maha", "Age": 15, "Gender": "f"},
-                {"id": 3, "Name": "Ali", "Age": 40, "Gender": "m"},
-                {"id": 4, "Name": "Tahir", "Age": 63, "Gender": 'm'},
-                {"id": 5, "Name": "Ayesha", "Age": 25, "Gender": "f"},
-                {"id": 6, "Name": "Zahid", "Age": 23, "Gender": "m"},
-             ]
+with open("users.json", "r") as json_file:
+    new_users = json.load(json_file)
 
 
-@app.route("/home")
+@app.route("/")
 def home():
     return "Home Route"
 
 
-@app.get("/Users_Information")
-def user_information(data=users):
-    return jsonify(data)
-
-
-@app.route("/User_info", methods=['POST'])
-def user_data2():
-    data = request.get_json()
-    new_data = {"Name": data['Name'], "Age": data["Age"], "Gender": data["Gender"]}
-    return jsonify(new_data)
+@app.route("/Users_Information")
+def user_information():
+    return new_users
 
 
 @app.route("/Authorization", methods=['POST'])
@@ -42,15 +30,35 @@ def authorize_user():
     new_data = {"Name": data['Name'], "Age": data["Age"], "Gender": data["Gender"]}
     api = request.headers.get("Authorization")
     if api and api != API_KEY:
-        return "Unauthorized User"
+        return "Unauthorized access"
     elif not api:
         return "API KEY needed"
     elif api == API_KEY:
-        session_id = uuid.uuid4()
-        new_data["session_id"] = session_id
-        session_file.update(new_data)
-        users.append(new_data)
+        session_id = str(uuid.uuid4())
+        new_data["id"] = session_id
+        new_list = list(new_users)
+        new_list.append(new_data)
+        with open("users.json", "w") as json_new_file:
+            json.dump(new_list, json_new_file, indent=4)
         return f"Authorization Successful : Your Session_id is {session_id}"
+
+
+@app.route("/Get_User_Information", methods=["POST"])
+def get_user_data():
+    api = request.headers.get("Authorization")
+    user_id = request.get_json()
+    if api and api != API_KEY:
+        return "Unauthorized access"
+    elif not api:
+        return "API Key needed"
+    elif api == API_KEY:
+        with open("users.json", "r") as new_file:
+            users = json.load(new_file)
+        for i in users:
+            if i["id"] == user_id["id"]:
+                return {"Name": i["Name"], "Age": i["Age"], "Gender": i["Gender"]}
+                break
+        return "Session_id does not exist"
 
 
 if __name__ == "__main__":
