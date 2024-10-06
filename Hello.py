99c04b9defffefb4ab97c1a282a1cfe0d9693e82
Nirwa22 <nyusra.msai23seecs@seecs.edu.pk>
2024-10-07 01:46:23 +0500

    Try and except added

--- Hello.py
+++ Hello.py
@@ -26,37 +26,45 @@ def user_information():
 
 @app.route("/Authorization", methods=['POST'])
 def authorize_user():
-    data = request.get_json()
     api = request.headers.get("Authorization")
     if api and api != API_KEY:
         return "Unauthorized access"
     elif not api:
         return "API KEY needed"
     elif api == API_KEY:
-        session_id = str(uuid.uuid4())
-        new_data = {"id": session_id, "Name": data['Name'], "Age": data["Age"], "Gender": data["Gender"]}
-        new_users.append(new_data)
-        with open("users.json", "w") as json_new_file:
-            json.dump(new_users, json_new_file, indent=4)
-        return f"Authorization Successful : Your Session_id is {session_id}"
+        try:
+            data = request.get_json()
+            session_id = str(uuid.uuid4())
+            new_data = {"id": session_id, "Name": data['Name'], "Age": data["Age"], "Gender": data["Gender"]}
+            new_users.append(new_data)
+            with open("users.json", "w") as json_new_file:
+                json.dump(new_users, json_new_file, indent=4)
+            return f"Authorization Successful : Your Session_id is {session_id}"
+        except Exception:
+            return "Message: Information missing."
 
 
 @app.route("/Get_User_Information", methods=["POST"])
 def get_user_data():
     api = request.headers.get("Authorization")
-    user_id = request.get_json()
     if api and api != API_KEY:
         return "Unauthorized access"
     elif not api:
         return "API Key needed"
     elif api == API_KEY:
-        with open("users.json", "r") as new_file:
-            users = json.load(new_file)
-        for i in users:
-            if i["id"] == user_id["id"]:
-                return {"Name": i["Name"], "Age": i["Age"], "Gender": i["Gender"]}
-                break
-        return "Session_id does not exist"
+        try:
+            user_id = request.get_json()
+            with open("users.json", "r") as new_file:
+                users = json.load(new_file)
+            for i in users:
+                if user_id["id"] and i["id"] == user_id["id"]:
+                    return {"Name": i["Name"], "Age": i["Age"], "Gender": i["Gender"]}
+                    break
+                elif not user_id["id"]:
+                    return "Please enter the ID"
+            return "Session_id does not exist"
+        except Exception:
+            return "Message: Information missing"
 
 
 if __name__ == "__main__":
